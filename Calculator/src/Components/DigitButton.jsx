12c8ca68868cbd3353f9f3c85fcefe47d194c0ba
Denis Yordanov <deyordanov777@gmail.com>
2023-10-16 20:14:35 +0300

    Added A New Exercise

---
+++ Calculator/src/Components/DigitButton.jsx
@@ -0,0 +1,17 @@
+import { ACTIONS } from "../App";
+import PropTypes from "prop-types";
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
+
+DigitButton.propTypes = {
+  dispatch: PropTypes.func,
+  digit: PropTypes.string,
+};
